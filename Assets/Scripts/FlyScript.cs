ae83a20b08505dfb4d0089effe02a55492d4955f
Muhammad Imtiaz Ashiq <63284641+imtiazashiq@users.noreply.github.com>
2022-09-06 14:38:51 +0200

    Update FlyScript.cs

--- Assets/Scripts/FlyScript.cs
+++ Assets/Scripts/FlyScript.cs
@@ -26,21 +26,7 @@ private void Start()
 pos = transform.position;
 sc.tilecollider = true;
 sc1.tilecollider = true;
-
-//GameObject orginalGameObject= GameObject.FindWithTag("mainplane");
- //sc = GameObject.FindGameObjectsWithTag("main").GetComponent<scrip>();
-//sc1 = GameObject.Find("righfoot").GetComponent<scrip1>();
-
- //GameObject child1 = GameObject.Find("Captury Plane/unknown(30004)/Capsule/rightfoot").gameObject;
-
-// sc= ob.GetComponent<scrip>();
- //sc = child.GetComponent<scrip>();
- 
- //sc1 = child1.GetComponent<scrip1>();
- 
- 
- 
- audio = GetComponent<AudioSource>();
+audio = GetComponent<AudioSource>();
  audio.Play();
  
  
@@ -50,24 +36,16 @@ void Update()
 {
 
  
- //sc1.tilecollider = false;
  if(move){
   
-  
-//calculate what the new Y position will be
-float newY = Mathf.Sin(Time.time * speed) * height + pos.x;
-//set the object's Y to the new calculated Y
+ float newY = Mathf.Sin(Time.time * speed) * height + pos.x;
 transform.position = new Vector3( newY,transform.position.y, transform.position.z) ;
 }
 if(!move)
 {
-         //  Destroy (gameObject, 3);
          
          Invoke("Destroy", 3);
          audio.Stop();
-         
-        
-
 }
 
 }
@@ -88,10 +66,6 @@ void OnCollisionExit(Collision collision)
  {
   Debug.Log("WE HIT AN OBSTACLE");
   move= false;
- // sc = collision.gameObject.GetComponent<scrip>();
- // sc1 = collision.gameObject.GetComponent<scrip1>();
-  //sc.tilecollider = true;
- //sc1.tilecollider = true;
   Destroy(gameObject);
  }
 }
@@ -99,10 +73,7 @@ void OnCollisionExit(Collision collision)
 public void Destroy()
 {
  Destroy(gameObject);
- Debug.Log("dessssssssss");
  sc.tilecollider = false;
  sc1.tilecollider = false;
- 
- //sc1.tilecollider = false;
 }
 }
